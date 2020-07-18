307ae4da1624edfccb7e6c3eef76436a9f7acb1b
Ivan <ivan.apple15@icloud.com>
2020-07-18 19:45:46 +0300

    Initial Commit

---
+++ CityWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityWeather
+//
+//  Created by Ivan on 18.07.2020.
+//  Copyright © 2020 Ivan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
